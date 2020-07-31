ea43684c0ed6103afc5524b36ca89d8de006beb3
RUAN-ZX <https://github.com/RUAN-ZX>
2020-08-01 02:01:07 +0800

    Getting good demo for reference:)

--- demo_xyf/pages/index/index.js
+++ demo_xyf/pages/index/index.js
@@ -5,14 +5,33 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        swiperList:[]
     },
 
     /**
-     * 生命周期函数--监听页面加载
+        * 访问失败的问题！！！
      */
     onLoad: function (options) {
+        wx.request({
+          url: 'https://www.baidufe.com/test-post.php',
+          data:{
+            username:"postman",
+            password:"123456"
+                
+          },
 
+          method:'post',
+
+          success:(result)=>{
+              this.setData({
+                  swiperList:[
+                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1596131381499&di=2a83933721b86e67486601c4a52eec39&imgtype=0&src=http%3A%2F%2Fpic.90sjimg.com%2Fback_pic%2Fqk%2Fback_origin_pic%2F00%2F03%2F74%2Fa5122f03dd68b38c0f344bc8f4d4065f.jpg",
+                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1596131418348&di=bc840efdbfa6bd717fc0a83b845675ae&imgtype=0&src=http%3A%2F%2Fpic.90sjimg.com%2Fback_pic%2Fqk%2Fback_origin_pic%2F00%2F04%2F14%2F8d59ffbfb93a77f291a48496e007e5ee.jpg",
+                      "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2696964805,314438294&fm=26&gp=0.jpg"
+                  ]
+              })
+          }
+        })
     },
 
     /**
