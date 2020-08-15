1775076b835d9e2798eaff4c7443c14f9e863a0e
1162179851@qq.com <3269469626@qq.com>
2020-08-16 01:16:46 +0800

    index page half done

---
+++ demo_xyf/components/list_info/list_info.js
@@ -0,0 +1,26 @@
+// components/list_info.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+        list_info_data:{
+            type:Array,
+            value:[],
+        }
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
--- demo_xyf/pages/index/index.js
+++ demo_xyf/pages/index/index.js
@@ -1,92 +1,178 @@
 // pages/index/index.js
 Page({
 
-    /**
-     * 页面的初始数据
-     * 
+  /**
+   * 页面的初始数据
+   * 
 *       wx.showTabBarRedDot({
-          index: 3,
-        });
-        wx.hideTabBarRedDot({
-          index: 0,
-        });
-     */
-    data: {
-        swiperList:[]
-    },
+        index: 3,
+      });
+      wx.hideTabBarRedDot({
+        index: 0,
+      });
+   */
+  data: {
+    swiperList: [
+      "../../jpg/1.jpg",
+      "../../jpg/2.jpg",
+      "../../jpg/3.jpg",
+    ],
+    div_1: [
+      {
+        "src": "../../jpg/icon_div_1/1.png",
+        "text": "恋爱心理",
+      },
+      {
+        "src": "../../jpg/icon_div_1/2.png",
+        "text": "压力疏导",
+      },
+      {
+        "src": "../../jpg/icon_div_1/3.png",
+        "text": "我要倾诉",
+      },
+      {
+        "src": "../../jpg/icon_div_1/4.png",
+        "text": "婚姻关系",
+      },
+      {
+        "src": "../../jpg/icon_div_1/5.png",
+        "text": "亲子教育",
+      },
+      {
+        "src": "../../jpg/icon_div_1/6.png",
+        "text": "成长解惑",
+      },
+      {
+        "src": "../../jpg/icon_div_1/7.png",
+        "text": "人际关系",
+      },
+      {
+        "src": "../../jpg/icon_div_1/8.png",
+        "text": "抑郁疏导",
+      },
+      {
+        "src": "../../jpg/icon_div_1/9.png",
+        "text": "职场心理",
+      },
+      {
+        "src": "../../jpg/icon_div_1/10.png",
+        "text": "法律咨询",
+      }
+    ],
+    div_2: [
+      "../../jpg/icon_div_2/1.jpg",
+      "../../jpg/icon_div_2/2.jpg",
+      "../../jpg/icon_div_2/3.jpg",
+      "../../jpg/icon_div_2/4.jpg",
 
-    /**
-        * 访问失败的问题！！！
-     */
-    onLoad: function (options) {
-        wx.request({
-          url: 'https://www.baidufe.com/test-post.php',
-          data:{
-            username:"postman",
-            password:"123456"
-                
+    ],
+    tabs: [
+      {
+        id: 0,
+        name: "恋爱心理  ",
+        isActive: true
+      },
+      {
+        id: 1,
+        name: "压力疏导  ",
+        isActive: false
+      },
+      {
+        id: 2,
+        name: "我要倾诉  ",
+        isActive: false
+      },
+      {
+        id: 3,
+        name: "婚姻关系  ",
+        isActive: false
+      },
+
+      {
+        id: 4,
+        name: "亲子教育  ",
+        isActive: false
+      },
+      {
+        id: 5,
+        name: "成长解惑  ",
+        isActive: false
+      },
+      {
+        id: 6,
+        name: "人际关系  ",
+        isActive: false
+      },/*
+          {
+            id:7,
+            name:"抑郁疏导",
+            isActive:false
           },
+          {
+            id:8,
+            name:"职场心理",
+            isActive:false
+          },
+          {
+            id:9,
+            name:"法律咨询",
+            isActive:false
+          },
+          */
+    ],
 
-          method:'post',
-
-          success:(result)=>{
-              this.setData({
-                  swiperList:[
-                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1596131381499&di=2a83933721b86e67486601c4a52eec39&imgtype=0&src=http%3A%2F%2Fpic.90sjimg.com%2Fback_pic%2Fqk%2Fback_origin_pic%2F00%2F03%2F74%2Fa5122f03dd68b38c0f344bc8f4d4065f.jpg",
-                      "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1596131418348&di=bc840efdbfa6bd717fc0a83b845675ae&imgtype=0&src=http%3A%2F%2Fpic.90sjimg.com%2Fback_pic%2Fqk%2Fback_origin_pic%2F00%2F04%2F14%2F8d59ffbfb93a77f291a48496e007e5ee.jpg",
-                      "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=2696964805,314438294&fm=26&gp=0.jpg"
-                  ]
-              })
-          }
-        })
+    cap_info: {
+      height: 0,
+      top: 0,
+      left: 0,
+      right: 0,
+      width: 0,
+      bottom: 0
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+    list_info_data:[
+      {
+        id:"111",//数据库ID
+        name:"阮智祥",
+        achieveLabel:["二级心理咨询师"], //头衔
+        expertLabel:["恋爱心理","压力疏导","我要倾诉"], //代表四个不同的领域 具体指代以首页顺序为主
+        price:150,
+        customNum:1314, //曾今帮助服务到的人的数量
+        fanNum:2333, //粉丝数量
+        timeNum:4023,//服务小时数
+        star:5, //评分
+        avatar:"../../jpg/avatar/1.jpg",
+      },
 
-    },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
+    ]
+  },
 
-    },
+  onReady: function () {
 
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
+    let menuButton = wx.getMenuButtonBoundingClientRect()
+    this.setData({
+      cap_info: {
+        height: menuButton.height,
+        right: menuButton.right,
+        left: menuButton.left,
+        top: menuButton.top,
+        width: menuButton.width,
+        bottom: menuButton.bottom
+      }
+    });
+    //console.log(cap_info);
+  },
+  handleTabsChange(e) {
+    const { index } = e.detail;
+    let tabs = JSON.parse(JSON.stringify(this.data.tabs));
 
-    },
+    tabs.forEach((v, i) => i === index ? v.isActive = true : v.isActive = false);
 
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
+    this.setData({
+      tabs
+    })
+  },
 
-    },
 
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
